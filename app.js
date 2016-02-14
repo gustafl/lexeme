c61ca979024433ec42eab72c1b12a007707d354f
gustafl <gustafl@bahnhof.se>
2016-02-14 03:31:43 +0100

    Drafting REST API.

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var api = require('./api.js')(app);
+
+var server = app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
